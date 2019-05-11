85a9e450a26aa73ca13692d935c147bd407ccd9e
guillermoams <ams.guillermo@gmail.com>
2019-05-11 15:53:08 -0500

    Refactoring

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,27 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Mail\LoanReminder;
-
 use App\Loan;
 use App\State;
-
 use App\Mail\CanceledLoanByProfessor;
-
 use Mail;
-
 use DB;
 
 class AboutController extends Controller
 {
-  public function viewAbout()
-  {
+  public function viewAbout() {
       return view('about');
   }
 
-  public function getInventory()
-  {
+  public function getInventory() {
     $devices = DB::select("
       SELECT COUNT(d.id) as quantity, d.name, d.brand, d.model
       FROM devices d JOIN states s
@@ -38,117 +31,100 @@ class AboutController extends Controller
   }
 
   public function acceptLoan($loanID){
-    
+
     $defaultMessage = "La responsabilidad del préstamo ha sido aceptada.";
-
-    // $loanID = $request->input('loanID');
-
-    $loanToCheck = DB::select("
-      SELECT status
-      FROM loans
-      WHERE id = '$loanID'
-    ");
+    $loanToCheck = $this->getLoanStatus($loanID);
 
     if(count($loanToCheck) != 0){
-      $statusToVerify = $loanToCheck[0]->status;
+      $loanStatus = $loanToCheck[0]->status;
 
       //Si el estado actual de el préstamo es cancelado no permirtir hacer la siguiente linea
-
-      if($statusToVerify == "Pending"){
-        
-        Loan::where('id', $loanID)->update(['status' => "New"]);
-        
-      }else{
-
+      if($loanStatus == "Pending") {
+        $this->setLoanStatus($loanID, "New");
+      } else {
         // Hacer else para cuando se tenga que devolver el mensaje [Hacer variable de mensaje y no dejar el mensaje de abajo estático]
-        
         $defaultMessage = "El préstamo seleccionado no es nuevo, la operación no es posible de realizar.";
-
       }
-
-    }else{
-
+    } else {
       $defaultMessage = "El préstamo seleccionado no ha podido ser cancelado, posiblemente ya fue cancelado. Verificar su estado en el buscador de préstamos.";
-
     }
-
     return view('loan-accepted')->with('loanID', $loanID)->with('message', $defaultMessage);
   }
 
   public function declineLoan($loanID){
-    
-    $defaultMessage = "El préstamo ha sido cancelado y los dispositivos han regresado a estar disponibles.";
 
-    $loanToCheck = DB::select("
+    $defaultMessage = "El préstamo ha sido cancelado y los dispositivos han regresado a estar disponibles.";
+    $loanToCheck = $this->getLoanStatus($loanID);
+
+    if(count($loanToCheck) != 0){
+
+      $loanStatus = $loanToCheck[0]->status;
+
+      if($loanStatus != "Cancelled"){
+        // Cancel loan
+        $this->setLoanStatus($loanID, "Cancelled");
+
+        // Release Devices
+        $devices = $this->getDevicesFromLoan($loanID);
+        $this->setDeviceStatus($devices, "Available");
+
+        // Send email
+        $applicant = $this->getAplicantFromLoan($loanID);
+        \Mail::to($applicant[0]->email)->send(new CanceledLoanByProfessor($loanID, $applicant[0]->name));
+
+      } else {
+        $defaultMessage = "El préstamo seleccionado no ha podido ser cancelado, posiblemente ya fue cancelado. Verificar su estado en el buscador de préstamos.";
+      }
+    } else {
+      $defaultMessage = "Operación no permitida, el préstamo no se encuentra registrado.";
+    }
+    return view('loan-declined')->with('loanID', $loanID)->with('message', $defaultMessage);
+  }
+
+  private function setLoanStatus($loanID, $newStatus) {
+    Loan::where('id', $loanID)->update(['status' => $newStatus]);
+  }
+
+  public function setDeviceStatus($devices, $newStatus) {
+    foreach ($devices as $deviceID) {
+      State::where('device_id', $deviceID->id)->update(['state' => $newStatus]);
+    }
+  }
+
+  public function getDevicesFromLoan($loanID) {
+    $devices = DB::select("
+      SELECT d.id
+      FROM loans l JOIN loan_device ld ON l.id = ld.loan_id
+      JOIN devices d ON d.id = ld.device_id
+      JOIN states s ON s.device_id = d.id
+      WHERE l.id = '$loanID';
+    ");
+
+    return $devices;
+  }
+
+  public function getAplicantFromLoan($loanID) {
+    $applicantInfo = DB::select("
+      SELECT a.name, a.email
+      FROM loans l
+      JOIN applicants a ON l.applicant_id = a.id
+      WHERE l.id = '$loanID';
+    ");
+
+    return $applicantInfo;
+  }
+
+  public function getLoanStatus($loanID) {
+    $loanStatus = DB::select("
       SELECT status
       FROM loans
       WHERE id = '$loanID'
     ");
 
-    if(count($loanToCheck) != 0){
-
-      $statusToVerify = $loanToCheck[0]->status;
-
-      if($statusToVerify != "Cancelled"){
-
-        Loan::where('id', $loanID)->update(['status' => "Cancelled"]);
-
-        $device_ids = DB::select("
-          SELECT d.id
-          FROM loans l JOIN loan_device ld ON l.id = ld.loan_id
-          JOIN devices d ON d.id = ld.device_id
-          JOIN states s ON s.device_id = d.id
-          WHERE l.id = '$loanID';
-        ");
-
-        foreach ($device_ids as $device_id) {
-          State::where('device_id', $device_id->id)->update(['state' => "Available"]);
-        }
-        
-        $emailToSendMessage = DB::select("
-          SELECT a.name, a.email
-          FROM loans l
-          JOIN applicants a ON l.applicant_id = a.id
-          WHERE l.id = '$loanID';
-        ");
-
-        \Mail::to($emailToSendMessage[0]->email)->send(new CanceledLoanByProfessor($loanID, $emailToSendMessage[0]->name));
-
-      }else{
-
-        $defaultMessage = "El préstamo seleccionado no ha podido ser cancelado, posiblemente ya fue cancelado. Verificar su estado en el buscador de préstamos.";
-
-      }
-
-    }else{
-      
-      $defaultMessage = "Operación no permitida, el préstamo no se encuentra registrado.";
-
-    }
-    
-
-    return view('loan-declined')->with('loanID', $loanID)->with('message', $defaultMessage);
-
+    return $loanStatus;
   }
 
   public function sendLoanReminders(){
-    
-    /*
-    Mail::send([], [], function($message){
-
-      $message->from('prueba@laravel.com', 'Prueba de Laravel');
-
-      $message->to('luis_alfonso_96@hotmail.com');
-
-      $message->replyTo('luisandroid09@gmail.com', 'Luis Rojo');
-
-      $message->subject('Prueba para ver si llega el mensaje');
-
-      $message->setBody('Llegó', 'text/html');
-
-    });
-    */
-
     $remaindersToSend = DB::select("
       SELECT l.id, l.end_date, ap.name, ap.email
       FROM loans l
@@ -158,11 +134,8 @@ class AboutController extends Controller
     ");
 
     foreach ($remaindersToSend as $remainderToSend) {
-      
       \Mail::to($remainderToSend->email)->send(new LoanReminder($remainderToSend->id, $remainderToSend->end_date, $remainderToSend->name, $remainderToSend->email));
-      
     }
-
   }
 
 }
